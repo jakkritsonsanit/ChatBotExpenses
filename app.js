9b41c8c2db44fb63f761ad5d8fb3e474f2482246
Jakkrit <palm8ikaa2@gmail.com>
2019-11-22 00:53:12 +0700

    Daily Update

--- app.js
+++ app.js
@@ -10,20 +10,37 @@
     firebase.initializeApp(config);
 
     // Get element
-    const preObject = document.getElementById('object');
+    const preObject_expenses = document.getElementById('object_expenses');
+    const preObject_income = document.getElementById('object_income');
 
     // Create references
-    const dbRefObject = firebase.database().ref().child('expenses');
+    const dbRefObject_expenses = firebase.database().ref().child('expenses');
+    const dbRefObject_income = firebase.database().ref().child('income');
 
     // Sync object changes
-    dbRefObject.on('value', snap => {
-      var amt = 0
+    var expenses = 0
+    var income = 0
+    console.log(income)
+    
+    dbRefObject_expenses.once('value', snap => {
+      
       snap.forEach(function(transaction) {
-        amt = amt + transaction.val().money;
+        expenses = expenses + transaction.val().money;
       });
-      preObject.innerText = amt;
+      console.log(expenses)
+      preObject_expenses.innerText = "Expenses : ".concat(expenses);
     });
     
+    dbRefObject_income.once('value', snap => {
+      
+      snap.forEach(function(transaction) {
+        income = income + transaction.val().money;
+      });
+      console.log(income)
+      preObject_income.innerText = "Income : ".concat(income);
+      document.getElementById('object_total').innerText = "Total : ".concat((income - expenses));
+    });
+
   }());
 // preObject.innerText = JSON.stringify(snap.val(), null, 3);
 // for each ตรง snap ได้เลย 
